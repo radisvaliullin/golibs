555f997ce833e47dce42d575c1768fa71beb2510
Radis Valiullin <radisvaliullin@gmail.com>
2018-11-11 16:10:06 +0300

    scheduler: test delay

--- sched/job_test.go
+++ sched/job_test.go
@@ -8,8 +8,8 @@ import (
 
 func TestJob_PeriodRun(t *testing.T) {
 
-	send := make(chan struct{})
-	recv := make(chan struct{})
+	send := make(chan struct{}, 1)
+	recv := make(chan struct{}, 1)
 
 	f := func(ctx context.Context) error {
 		<-send
@@ -18,22 +18,68 @@ func TestJob_PeriodRun(t *testing.T) {
 	}
 
 	j := NewJob("id", 100, 100, 0, f)
-
 	j.Start(context.Background())
 
 	send <- struct{}{}
+	tm0 := time.NewTimer(time.Millisecond * 150)
+	tm1 := time.NewTimer(time.Millisecond * 300)
 	select {
 	case <-recv:
 		t.Logf("send recv done")
-	case <-time.NewTimer(time.Millisecond * 200).C:
+	case <-tm0.C:
 		t.Fatalf("send recv fail")
 	}
 	send <- struct{}{}
 	select {
 	case <-recv:
 		t.Logf("send recv done")
-	case <-time.NewTimer(time.Millisecond * 200).C:
+	case <-tm1.C:
 		t.Fatalf("send recv fail")
+	}
+}
+
+func TestJob_DelayRun(t *testing.T) {
+
+	// without delay
+	send0 := make(chan struct{}, 1)
+	recv0 := make(chan struct{}, 1)
+	f0 := func(ctx context.Context) error {
+		<-send0
+		recv0 <- struct{}{}
+		return nil
+	}
+
+	j0 := NewJob("id0", 100, 100, 0, f0)
+	j0.Start(context.Background())
+
+	send0 <- struct{}{}
+	tm0 := time.NewTimer(time.Millisecond * 150)
+	select {
+	case <-recv0:
+		t.Logf("send recv done")
+	case <-tm0.C:
+		t.Fatalf("send recv fail")
+	}
+
+	// with delay
+	send1 := make(chan struct{}, 1)
+	recv1 := make(chan struct{}, 1)
+	f1 := func(ctx context.Context) error {
+		<-send1
+		recv1 <- struct{}{}
+		return nil
+	}
+
+	j1 := NewJob("id1", 150, 150, 150, f1)
+	j1.Start(context.Background())
+
+	send1 <- struct{}{}
+	tm1 := time.NewTimer(time.Millisecond * 100)
+	select {
+	case <-recv1:
+		t.Fatal("with delay, send recv done before delay time")
+	case <-tm1.C:
+		t.Logf("send recv, delay ok")
 	}
 }
 
